734aa1176f5e21279327d3dba092ee8b63bf1cae
hrk fkm <fkm@fkmsoft.jp>
2015-03-03 13:45:16 +0900

    Initial commit

---
+++ mail_test.go
@@ -0,0 +1,22 @@
+package mailgun
+
+import (
+	"testing"
+)
+
+const (
+	apiKey  = "<Input your API Key>"
+	domain  = "<Input your domain in mailgun>"
+	from    = "<Input from address>"
+	to      = "<Input to address>"
+	subject = "メールガンから送信"
+	body    = "テストコードから本文"
+)
+
+func Test_OK(t *testing.T) {
+	client := New(apiKey, domain, from)
+	err := client.Send(to, subject, body)
+	if err != nil {
+		t.Errorf("Unexepcted Error : %s", err)
+	}
+}
